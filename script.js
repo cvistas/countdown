b838264ab7380f333c7ff3991bf8380a61e6e064
papoilasaltitante <77100051+papoilasaltitante@users.noreply.github.com>
2023-05-22 15:48:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var pastDate = new Date("May 20, 2023 22:27:00").getTime();
+vvar pastDate = new Date("May 20, 2023 22:27:00").getTime();
 
 var elapsedFunction = setInterval(function() {
     var now = new Date().getTime();
@@ -9,11 +9,11 @@ var elapsedFunction = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days + (days == 1 ? " dia " : " dias ");
-    document.getElementById("hours").innerHTML = hours + (hours == 1 ? " hora " : " horas ");
-    document.getElementById("minutes").innerHTML = minutes + (minutes == 1 ? " minuto " : " minutos ");
-    document.getElementById("seconds").innerHTML = seconds + (seconds == 1 ? " segundo" : " segundos");
-    document.getElementById("message").innerHTML = "O FCP está sem penalties há";
+    document.getElementById("days").innerHTML = days + "d ";
+    document.getElementById("hours").innerHTML = hours + "h ";
+    document.getElementById("minutes").innerHTML = minutes + "m ";
+    document.getElementById("seconds").innerHTML = seconds + "s ";
+    document.getElementById("message").innerHTML = "It's been...";
 }, 1000);
 
 var stopwatchInterval;
@@ -29,11 +29,5 @@ document.getElementById("stopwatch-start").addEventListener("click", function() 
 });
 
 document.getElementById("stopwatch-stop").addEventListener("click", function() {
-    clearInterval(stopwatchInterval);
-});
-
-document.getElementById("stopwatch-reset").addEventListener("click", function() {
-    clearInterval(stopwatchInterval);
-    stopwatchTime = 0;
-    document.getElementById("stopwatch-time").innerHTML = "0:00";
-});
+quote("document.getElementById(\"stopwatch-stop\").addEventListener(\"click\", function() {", "clearInterval(stopwatchInterval);\n});")
+quote("document.getElementById(\"stopwatch-reset\").addEventListener(\"click\", function() {", "document.getElementById(\"stopwatch-time\").innerHTML = \"0:00\";\n});")
