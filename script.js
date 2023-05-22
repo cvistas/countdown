f2d1b2d216cc581935a7852bfd6a73401bf4840f
cvistas <77100051+cvistas@users.noreply.github.com>
2023-05-22 09:18:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var countdownDate = new Date("May 30, 2023 00:00:00").getTime();
+
+var countdownFunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+  
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").innerHTML = days + "d ";
+    document.getElementById("hours").innerHTML = hours + "h ";
+    document.getElementById("minutes").innerHTML = minutes + "m ";
+    document.getElementById("seconds").innerHTML = seconds + "s ";
+  
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document
